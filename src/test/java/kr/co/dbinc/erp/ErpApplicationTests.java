f2899c99bea27150490e8fb5fa74c72545d4ac3d
googoo9918 <1013as@naver.com>
2025-09-19 16:00:31 +0900

    Inital Commit

---
+++ src/test/java/kr/co/dbinc/erp/ErpApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.dbinc.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
