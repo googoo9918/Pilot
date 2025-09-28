eaf6c234e7837587a1566508f012373e9dc469a0
googoo9918 <1013as@naver.com>
2025-09-29 08:34:43 +0900

    Fix: 패키지명 수정(erp -> com)

--- src/main/java/kr/co/dbinc/com/ErpApplication.java
+++ src/main/java/kr/co/dbinc/com/ErpApplication.java
@@ -1,4 +1,4 @@
-package kr.co.dbinc.erp;
+package kr.co.dbinc.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/kr/co/dbinc/com/ErpApplicationTests.java
+++ src/test/java/kr/co/dbinc/com/ErpApplicationTests.java
@@ -1,4 +1,4 @@
-package kr.co.dbinc.erp;
+package kr.co.dbinc.com;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
